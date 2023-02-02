42ae769df715eef5f447898cf12e6ecb35b1286e
alessandrob17 <a.baldipardi@gmail.com>
2023-02-02 15:31:48 +0100

    FEAT fizzbuzz

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-// // // Scrivi un programma che stampi in console i numeri da 1 a 100,
-// // // ma che per i multipli di 3 stampi “Fizz” al posto del numero
-// // // e per i multipli di 5 stampi “Buzz”.
-// // // // Per i numeri che sono sia multipli di 3 che di 5 stampi (quindi multipli di 15) “FizzBuzz”.
-
-
-for(let i= 0; i <100; i++){
-    let number= i + 1
-    console.log(number)
-}
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } 
+  }
+  
